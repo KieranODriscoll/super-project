ca403e7782c46468b53ba28807f4997797a0ba87
Kieran O'Driscoll <github@kodriscoll.com>
2025-07-30 19:42:55 -0400

    add groundwork for cloudflare tunnel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
+    allowedHosts: ['app.kodriscoll.xyz'],
     port: 3000,
     watch: {
       usePolling: true
